0390d24c183415b03639782390456230ec1e8782
Dey880 <day4y3@proton.me>
2024-10-09 10:26:37 +0200

    It works :D

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const path = require("path")
 const session = require("express-session")
 
 
-
 mongoose
 .connect("mongodb://127.0.0.1:27017/helpdesk").then(() => {
   console.log("Mongodb connected!")
